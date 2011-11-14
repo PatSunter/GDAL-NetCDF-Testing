9b51fbf328d7a151023458756d400375c2b65d4e
Patrick Sunter <pds@rsa-ula-dev.(none)>
2011-11-15 10:22:40 +1100

    Updated test script to use new FORMAT option instead of FILETYPE.

--- write_nc4_compress_testing/myTest.sh
+++ write_nc4_compress_testing/myTest.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-export CUSTOM_GDAL_PATH="/home/pds/+Codes/gdal_trunk_netcdfWork-git-nc4/gdal"
-. $CUSTOM_GDAL_PATH/update_path.sh
-gdal_translate -of netCDF -co FILETYPE=NC melb-small.tif melb-small-nc.nc
-gdal_translate -of netCDF -co FILETYPE=NC2 melb-small.tif melb-small-nc2.nc
-gdal_translate -of netCDF -co FILETYPE=NC4 melb-small.tif melb-small-nc4.nc
-gdal_translate -of netCDF -co FILETYPE=NC4C melb-small.tif melb-small-nc4c.nc
-gdal_translate -of netCDF -co FILETYPE=NC4C -co COMPRESS=deflate -co ZLEVEL=1\
+gdal_translate -of netCDF -co FORMAT=NC melb-small.tif melb-small-nc.nc
+gdal_translate -of netCDF -co FORMAT=NC2 melb-small.tif melb-small-nc2.nc
+gdal_translate -of netCDF -co FORMAT=NC4 melb-small.tif melb-small-nc4.nc
+gdal_translate -of netCDF -co FORMAT=NC4C melb-small.tif melb-small-nc4c.nc
+gdal_translate -of netCDF -co FORMAT=NC4C -co COMPRESS=deflate -co ZLEVEL=1\
     melb-small.tif melb-small-nc4c-z1.nc
-gdal_translate -of netCDF -co FILETYPE=NC4C -co COMPRESS=deflate -co ZLEVEL=5\
+gdal_translate -of netCDF -co FORMAT=NC4C -co COMPRESS=deflate -co ZLEVEL=5\
     melb-small.tif melb-small-nc4c-z5.nc
-gdal_translate -of netCDF -co FILETYPE=NC4C -co COMPRESS=deflate -co ZLEVEL=9\
+gdal_translate -of netCDF -co FORMAT=NC4C -co COMPRESS=deflate -co ZLEVEL=9\
     melb-small.tif melb-small-nc4c-z9.nc
