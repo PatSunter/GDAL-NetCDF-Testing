b87f7145b14488b651c996f340569bef05abda6d
Patrick Sunter <PatDevelop@gmail.com>
2011-11-29 11:32:20 +1100

    Updated to new GDAL options.

--- write_nc4_compress_testing/bigTest.sh
+++ write_nc4_compress_testing/bigTest.sh
@@ -1,3 +1,3 @@
-/usr/bin/time -f "%E, %P, %M"  gdal_translate -of netCDF -co FILETYPE=NC L71092086_08620110129_B10.TIF L71092086_08620110129_B10-nc.nc
-/usr/bin/time -f "%E, %P, %M"   gdal_translate -of netCDF -co FILETYPE=NC4C -co COMPRESS=deflate -co ZLEVEL=1 L71092086_08620110129_B10.TIF L71092086_08620110129_B10-nc4c-z1.nc
-/usr/bin/time -f "%E, %P, %M"   gdal_translate -of netCDF -co FILETYPE=NC4C -co COMPRESS=deflate -co ZLEVEL=9 L71092086_08620110129_B10.TIF L71092086_08620110129_B10-nc4c-z9.nc
+/usr/bin/time -f "%E, %P, %M"  gdal_translate -of netCDF -co FORMAT=NC L71092086_08620110129_B10.TIF L71092086_08620110129_B10-nc.nc
+/usr/bin/time -f "%E, %P, %M"   gdal_translate -of netCDF -co FORMAT=NC4C -co COMPRESS=deflate -co ZLEVEL=1 L71092086_08620110129_B10.TIF L71092086_08620110129_B10-nc4c-z1.nc
+/usr/bin/time -f "%E, %P, %M"   gdal_translate -of netCDF -co FORMAT=NC4C -co COMPRESS=deflate -co ZLEVEL=9 L71092086_08620110129_B10.TIF L71092086_08620110129_B10-nc4c-z9.nc
